7bd74b6f5c31de4540b20eb6a38cd9b118ab1441
sif33iya55 <133092200+sif33iya55@users.noreply.github.com>
2023-09-02 20:04:54 +0300

    Create Bank Account.cpp

---
+++ Bank Account.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+class BankAccount {
+private:
+    string accountNumber;
+    string accountHolderName;
+    double accountBalance;
+
+public:
+    BankAccount(string number, string name, double balance) {
+        accountNumber = number;
+        accountHolderName = name;
+        accountBalance = balance;
+    }
+
+    void deposit(double amount) {
+        accountBalance += amount;
+    }
+
+    void withdraw(double amount) {
+        if (amount <= accountBalance) {
+            accountBalance -= amount;
+        } else {
+            cout << "Insufficient balance!" << endl;
+        }
+    }
+
+    void displayAccount() {
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Account Holder Name: " << accountHolderName << endl;
+        cout << "Account Balance: " << accountBalance << endl;
+    }
+};
+
+int main() {
+    BankAccount account("123456789", "John Doe", 1000.0);
+
+    account.displayAccount();
+
+    account.deposit(500.0);
+    account.withdraw(200.0);
+
+    account.displayAccount();
+
+    return 0;
+}
